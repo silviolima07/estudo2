9a066d980c6e27dae2a76eda8235f8a12a16f851
silviolima07 <silviolima07@gmail.com>
2022-05-11 13:06:49 -0300

    teste

--- app.py
+++ app.py
@@ -82,16 +82,18 @@ def full_app():
 
     with st.echo("below"):
         # Specify canvas parameters in application
-        drawing_mode = st.sidebar.selectbox(
-            "Drawing tool:",
-            ("freedraw", "line", "rect", "circle", "transform", "polygon", "point"),
-        )
-        stroke_width = st.sidebar.slider("Stroke width: ", 1, 25, 3)
+        drawing_mode = "freedraw"
+        #st.sidebar.selectbox(
+        #    "Drawing tool:",
+        #    ("freedraw", "line", "rect", "circle", "transform", "polygon", "point"),
+        #)
+        stroke_width = 10 #st.sidebar.slider("Stroke width: ", 1, 25, 3)
         if drawing_mode == 'point':
             point_display_radius = st.sidebar.slider("Point display radius: ", 1, 25, 3)
-        stroke_color = st.sidebar.color_picker("Stroke color hex: ")
-        bg_color = st.sidebar.color_picker("Background color hex: ", "#eee")
-        bg_image = st.sidebar.file_uploader("Background image:", type=["png", "jpg"])
+            
+        stroke_color = "rgba(255,255,255)"#st.sidebar.color_picker("Stroke color hex: ")
+        bg_color = "rgba(0,0,0)"#st.sidebar.color_picker("Background color hex: ", "#eee")
+        bg_image =  'png' #st.sidebar.file_uploader("Background image:", type=["png", "jpg"])
         realtime_update = st.sidebar.checkbox("Update in realtime", True)
 
         # Create a canvas component
@@ -102,7 +104,7 @@ def full_app():
             background_color=bg_color,
             background_image=Image.open(bg_image) if bg_image else None,
             update_streamlit=realtime_update,
-            height=150,
+            height=500,
             drawing_mode=drawing_mode,
             point_display_radius=point_display_radius if drawing_mode == 'point' else 0,
             display_toolbar=st.sidebar.checkbox("Display toolbar", True),
@@ -146,7 +148,7 @@ def center_circle_app():
         initial_drawing=saved_state
         if st.sidebar.checkbox("Initialize with saved state", False)
         else None,
-        height=400,
+        height=500,
         width=600,
         drawing_mode="circle",
         key="center_circle_app",
