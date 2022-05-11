a30c31d3cba17b71c3e600c8415c8999a55d31e5
silviolima07 <silviolima07@gmail.com>
2022-05-11 18:16:48 -0300

    teste

--- app.py
+++ app.py
@@ -103,61 +103,7 @@ def full_app():
 
 
 def png_export():
-    st.markdown(
-        """
-    Realtime update is disabled for this demo. 
-    Press the 'Download' button at the bottom of canvas to update exported image.
-    """
-    )
-    try:
-        Path("tmp/").mkdir()
-    except FileExistsError:
-        pass
-
-    # Regular deletion of tmp files
-    # Hopefully callback makes this better
-    now = time.time()
-    N_HOURS_BEFORE_DELETION = 1
-    for f in Path("tmp/").glob("*.png"):
-        st.write(f, os.stat(f).st_mtime, now)
-        if os.stat(f).st_mtime < now - N_HOURS_BEFORE_DELETION * 3600:
-            Path.unlink(f)
-
-    if st.session_state["button_id"] == "":
-        st.session_state["button_id"] = re.sub(
-            "\d+", "", str(uuid.uuid4()).replace("-", "")
-        )
-
-    button_id = st.session_state["button_id"]
-    file_path = f"tmp/{button_id}.png"
-
-    custom_css = f""" 
-        <style>
-            #{button_id} {{
-                display: inline-flex;
-                align-items: center;
-                justify-content: center;
-                background-color: rgb(0,0,0);
-                color: rgb(255,255,255);
-                padding: .25rem .75rem;
-                position: relative;
-                text-decoration: none;
-                border-radius: 4px;
-                border-width: 1px;
-                border-style: solid;
-                border-color: rgb(230, 234, 241);
-                border-image: initial;
-            }} 
-            #{button_id}:hover {{
-                border-color: rgb(246, 51, 102);
-                color: rgb(246, 51, 102);
-            }}
-            #{button_id}:active {{
-                box-shadow: none;
-                background-color: rgb(246, 51, 102);
-                color: white;
-                }}
-        </style> """
+    
 
     data = st_canvas(update_streamlit=False, key="png_export")
     if data is not None and data.image_data is not None:
@@ -168,17 +114,14 @@ def png_export():
         buffered = BytesIO()
         im.save(buffered, format="PNG")
         img_data = buffered.getvalue()
-        try:
-            # some strings <-> bytes conversions necessary here
-            b64 = base64.b64encode(img_data.encode()).decode()
-        except AttributeError:
-            b64 = base64.b64encode(img_data).decode()
-
-        dl_link = (
-            custom_css
-            + f'<a download="{file_path}" id="{button_id}" href="data:file/txt;base64,{b64}">Export PNG</a><br></br>'
-        )
-        st.markdown(dl_link, unsafe_allow_html=True)
+        
+        if st.button("Prever")and canvas_result.image_data is not None:
+             
+                st.title("Previsão")
+                 
+                pred = modelo_keras.predict(img_normalizado)
+                st.write(pred)
+                st.title(pred.argmax())
 
 
 
